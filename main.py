2c5340a2cd45712301e9e05ec05ed7b38e01ae86
favreje <favreje@gmail.com>
2025-06-20 12:48:37 -0500

    Added message roles

--- main.py
+++ main.py
@@ -2,40 +2,58 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+
+def generate_content(client, messages, user_prompt, is_verbose):
+    # Retrieve a response and display it, along with token count metadata
+    model = "gemini-2.0-flash-001"
+    response = client.models.generate_content(
+        model=model,
+        contents=messages,
+    )
+
+    print("-" * 100)
+    print("AI Agent")
+    print("-" * 100)
+    print(response.text)
+    print("-" * 100)
+
+    if is_verbose:
+        print(f"Model: {model}")
+        print(f"User prompt: {user_prompt}")
+        if response.usage_metadata:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        else:
+            print("Usage metadata not available")
 
 
 def main():
-    # Get prompt from user. If not prompt provided, exit gracefully without calling the API
+    # Get prompt from user. If no prompt is provided, exit without calling the API
     args = sys.argv[1:]
     if not args:
         print("Gemini AI Code Assistant")
         print('\nUsage: py main.py "your prompt here"')
         sys.exit(1)
-    prompt = " ".join(args)
+    is_verbose = "--verbose" in args
+    prompt_args = [arg for arg in args if not arg.startswith("--")]
+    user_prompt = " ".join(prompt_args)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     # Create an instance of the Gemini client
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    # Retrieve a response and display it, along with token count metadata
-    model = "gemini-2.0-flash-001"
-    response = client.models.generate_content(model=model, contents=prompt)
-    print("-" * 100)
-    print("AI Agent")
-    print("-" * 100)
-    print(f"Model: {model}")
-    print(f"Prompt: {prompt}")
-    print()
-    print(response.text)
-
-    print()
-    print("-" * 100)
-    if response.usage_metadata:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        print("Usage metadata not available")
+    generate_content(
+        client=client,
+        messages=messages,
+        user_prompt=user_prompt,
+        is_verbose=is_verbose,
+    )
 
 
 if __name__ == "__main__":
