d8419c2e5ecdafb537f6bec2c449aaccfadde798
favreje <favreje@gmail.com>
2025-09-19 15:40:04 -0500

    added additional schemas and modified prompt

--- functions/schemas.py
+++ functions/schemas.py
@@ -1,3 +1,4 @@
+from typing import Type
 from google.genai import types
 
 
@@ -15,9 +16,68 @@ schema_get_files_info = types.FunctionDeclaration(
     ),
 )
 
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Returns the contents of a specified file within the working directory, truncated, if necessary, to 10,000 characters",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file, within the working directory, to read contents from.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Creates a subprocess that runs a specified Python file within the working directory, and returns the output from the interpreter.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The Python file to execute within the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="Optional command line arguments to pass to the Python file.",
+                ),
+                description="Optional command line arguments to pass to the Python file.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes a specified string to a designated file within the working directory. If a file does not exist, it creates one. If a file does exist, it overwrites the file's contents.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file, within the working directory, to write contents to.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="A string representing the contents to be written.",
+            ),
+        },
+        required=["file_path", "content"],
+    ),
+)
 
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
     ]
 )
